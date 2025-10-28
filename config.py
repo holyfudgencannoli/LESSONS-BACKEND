ce8e55ca0c160882f783f39d389d1729904bfdd0
holyfudgencannoli <moshimoshi8246@gmail.com>
2025-10-28 19:41:11 -0400

    Update DATABASE_URL to use PostgreSQL connection

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
 import os
 
 class Config:
-    DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///example.db")
+    DATABASE_URL = 'postgresql://lessons_db_7sii_user:RJZXEo09gpLpSru362xc8lotOEMHYbjd@dpg-d40l6o7diees73d8gaa0-a.oregon-postgres.render.com/lessons_db_7sii'
     CLOUDFLARE_R2_ACCOUNT_ID = os.getenv("R2_ACCOUNT_ID")
     CLOUDFLARE_R2_ACCESS_KEY_ID = os.getenv("R2_ACCESS_KEY_ID")
     CLOUDFLARE_R2_SECRET_ACCESS_KEY = os.getenv("R2_SECRET_ACCESS_KEY")
     CLOUDFLARE_R2_BUCKET = os.getenv("R2_BUCKET_NAME")
+
 
