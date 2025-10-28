40fddaddfb4f6bf21100f55b8f706dbfa92d274c
holyfudgencannoli <moshimoshi8246@gmail.com>
2025-10-28 19:38:40 -0400

    Remove unnecessary dotenv loading from config.py

--- config.py
+++ config.py
@@ -1,6 +1,4 @@
 import os
-from dotenv import load_dotenv
-load_dotenv()
 
 class Config:
     DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///example.db")
@@ -8,3 +6,4 @@ class Config:
     CLOUDFLARE_R2_ACCESS_KEY_ID = os.getenv("R2_ACCESS_KEY_ID")
     CLOUDFLARE_R2_SECRET_ACCESS_KEY = os.getenv("R2_SECRET_ACCESS_KEY")
     CLOUDFLARE_R2_BUCKET = os.getenv("R2_BUCKET_NAME")
+
